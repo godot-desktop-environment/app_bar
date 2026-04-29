1df04dfa5e0010fe4922788cd1c06c8246107f1f
thiagola92 <thiagola92@gmail.com>
2026-04-29 08:16:03 -0300

    Ability to execute new processes

---
+++ app_launcher/app_launcher.gd
@@ -0,0 +1,25 @@
+extends Button
+
+@export var path: String = "gnome-text-editor"
+
+@export var arguments: PackedStringArray = []
+
+var current_mouse_button: int = 0
+
+
+func _on_button_down() -> void:
+	current_mouse_button = Input.get_mouse_button_mask()
+
+
+func _on_button_up() -> void:
+	current_mouse_button = 0
+
+
+func _on_pressed() -> void:
+	if current_mouse_button & MOUSE_BUTTON_MASK_LEFT == MOUSE_BUTTON_MASK_LEFT:
+		OS.create_process(path, [])
+		print("left")
+	elif current_mouse_button & MOUSE_BUTTON_MASK_RIGHT == MOUSE_BUTTON_MASK_RIGHT:
+		print("right")
+	elif current_mouse_button & MOUSE_BUTTON_MASK_MIDDLE == MOUSE_BUTTON_MASK_MIDDLE:
+		print("middle")
