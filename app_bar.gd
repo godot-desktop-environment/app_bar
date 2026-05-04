7fbeb196e57e968c94ab88a67b618ac73fb16f7b
thiagola92 <thiagola92@gmail.com>
2026-05-04 10:05:34 -0300

    Switched to ScrollContainer

--- app_bar.gd
+++ app_bar.gd
@@ -1,29 +1,2 @@
 @tool
 extends PanelContainer
-
-@export var apps_size: int = 40:
-	set(s):
-		apps_size = s
-		_adjust_apps_size.call_deferred(s)
-
-
-func _adjust_apps_size(s: int) -> void:
-	%BarControls.custom_minimum_size.y = s
-	%Launcher.custom_minimum_size = Vector2(s, s)
-	%BarControls.size.y = 0
-	%Launcher.size.x = 0
-	
-	var controls_gap: int = %BarControls.get_theme_constant("separation")
-	var controls_count: int = %BarControls.get_child_count()
-	var apps_space: int = (
-		%BarControls.size.x -
-		%Launcher.size.x -
-		%SystemContainer.size.x -
-		((controls_count - 1) * controls_gap)
-	)
-	
-	%AppsContainer.adjust_apps_size.call_deferred(s, apps_space)
-
-
-func _on_resized() -> void:
-	_adjust_apps_size.call_deferred(apps_size)
